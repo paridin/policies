e9fccbd0611ffce763ba6b13c5ba4a8df3c4eb27
paridin <restrada@paridin.com>
2026-02-15 19:01:21 -0600

    update elixir to latest version

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Policies.MixProject do
       config_path: "config/config.exs",
       deps_path: "deps",
       lockfile: "mix.lock",
-      elixir: "~> 1.9",
+      elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
